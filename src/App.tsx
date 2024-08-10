c9688e525af3c9fc4da0517650373f3d3aa4e624
montaser77789 <montasergohar@gmail.com>
2024-08-10 11:51:47 +0300

    Upload project

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+import CertificateOfPurchase from './components/CertificateOfPurchase'
+
+function App() {
+
+  return (
+<>
+<CertificateOfPurchase />
+</>
+  )
+}
+
+export default App
